df51def725bffd4c5c4e3669cc485a77ad37d322
matthew-clay <alexandermike1999@gmail.com>
2022-01-18 11:14:44 +0630

    new branch master

--- toastAlert.js
+++ toastAlert.js
@@ -1,1 +1,25 @@
+const githubUrl = "https://github.com/matthew-clay";
+const responseText = "Link copied successfully to your Clipboard.";
 
+const buttonTag = document.querySelector("#button");
+const toastAlertContainerTag = document.querySelector(".toastAlertContainer");
+
+const createToastAlert = () => {
+  toastAlertContainerTag.innerHTML = "";
+  const toastAlertTag = document.createElement("div");
+  toastAlertTag.classList.add("toastAlert");
+  toastAlertTag.append(responseText);
+  toastAlertContainerTag.append(toastAlertTag);
+  toastAlertTag.style.bottom = `-${toastAlertTag.offsetHeight}px`;
+  setTimeout(() => {
+    toastAlertTag.style.bottom = 0;
+  }, 200);
+
+  setTimeout(() => {
+    toastAlertTag.style.bottom = `-${toastAlertTag.offsetHeight}px`;
+  }, 2000);
+};
+
+buttonTag.addEventListener("click", () => {
+  createToastAlert();
+});
